205eb9831a7d0c793ed3c4f5a8dad66efc685d0f
Caithllynn <caithllynn@gmail.com>
2026-03-26 22:09:57 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8767002441:AAGUZ7q4IREEcfXQytvONe6WELak_blGLdQ"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "System ready."
-    )
+    await update.message.reply_text("Bot aktif.")
+
+async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot masih hidup.")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("ping", ping))
 
 app.run_polling()
